5ec4782a55b06146eebaff8d5547aad52e3de263
josim78 <Sekhjasimuddin650@gmail.com>
2025-05-18 22:54:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "27483529"
 api_hash = "c3e9ea6320b861ad11e37c5260288bb3"
 bot_token = "7792141300:AAHMPlMIg4Ij9ni70L2soXx7n0cn5j8qDRw"
-auth_users = [5830856952]
+auth_users = [5840594311]
